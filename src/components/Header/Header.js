ff8f25d9bb0b0a74aea4ca3fa04817bd6801e8c1
AndresCalvoG <andrescalvo9407@gmail.com>
2023-01-20 08:43:20 -0800

    create a new title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <header className="header">
       <img src={logo} className="header-logo" alt="logo" />
-      <h1 className="header-title"> My Store</h1>
+      <h1 className="header-title"> My Store with Context</h1>
       <Counter />
     </header>
   );
